950a35e01202cc9ffff3cef52084901d9b60264e
Niels Gorter <gorterniels@linuxmail.org>
2022-10-07 15:08:08 +0200

    added shouldshow

--- lib/flutter_introduction_shared_preferences.dart
+++ lib/flutter_introduction_shared_preferences.dart
@@ -6,13 +6,26 @@ import 'package:shared_preferences/shared_preferences.dart';
 class SharedPreferencesIntroductionDataProvider extends IntroductionInterface {
   SharedPreferencesIntroductionDataProvider();
 
-  _writeKeyValue(String key, String value) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setString(key, value);
+  SharedPreferences? _prefs;
+  String key = '_completedIntroduction';
+
+  _writeKeyValue(String key, bool value) async {
+    _prefs!.setBool(key, value);
+  }
+
+  _init() async {
+    _prefs ??= await SharedPreferences.getInstance();
   }
 
   @override
   Future<void> setCompleted([bool value = true]) async {
-    await _writeKeyValue('_completedIntroduction', value.toString());
+    await _init();
+    _writeKeyValue(key, value);
+  }
+
+  @override
+  Future<bool> shouldShow() async {
+    await _init();
+    return _prefs!.getBool(key) ?? true;
   }
 }
